fd39aad1b571ae94d56170e41a2329cb848334fb
FaheemRafiq <faheemkhan198742@gmail.com>
2023-01-30 22:57:22 +0500

    some responsive

--- src/MultiSetupForm/MainLayout.vue
+++ src/MultiSetupForm/MainLayout.vue
@@ -4,10 +4,11 @@ import StepsList from "../components/StepsList.vue";
 
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-200">
-    <div class="p-3 border-1 bg-white rounded-lg flex">
+    <div class="p-3 border-1 bg-white rounded-lg flex lg:flex-row flex-col">
       <!-- Section One -->
       <div class="relative">
-        <img class="w-72" src="../assets/MultiImages/bg-sidebar-desktop.svg" alt="Side Bar Image">
+        <img class="hidden lg:inline-block w-72" src="../assets/MultiImages/bg-sidebar-desktop.svg" alt="Side Bar Image">
+        <img class="lg:hidden w-full" src="../assets/MultiImages/bg-sidebar-mobile.svg" alt="Side Bar Image">
         <section class="absolute top-7 left-5 text-white w-full">
           <div class="w-[90%] px-3">
             <StepsList />
@@ -15,7 +16,7 @@ import StepsList from "../components/StepsList.vue";
         </section>
       </div>
       <!-- Section Two -->
-      <div class="px-24 py-10 w-[700px]">
+      <div class="px-10 lg:px-24 py-5 lg:py-10 lg:w-[700px] relative xs:top-0 sm:-top-10 md:-top-24 lg:inset-0 bg-white">
         <RouterView />
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
     extend: {
       fontFamily:{
         Ubuntu : ['Ubuntu'],
+      },
+      screens:{
+        xs: '320px',
+        sm: '375px',
+        lsm: '425px',
+  
+        md: '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       }
     },
   },
